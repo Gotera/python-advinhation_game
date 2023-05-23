f522cc2def7a02dad1d2b993b341c97c450e5dcc
OM <orlandomedeiros.junior@gmail.com>
2023-05-22 21:01:44 -0300

    Just adjusts to 💫💫

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+print('__________________')
 print('Jogo de advinhação')
+print('▔▔▔▔▔▔▔▔▔▔')
 
 secret_number = 42
 kick_str = input('Digite o seu número: ')
@@ -11,3 +13,5 @@ if (secret_number == kick):
     print("Você acertou!")
 else:
     print("Você errou!")
+
+print("Final do Jogo")
