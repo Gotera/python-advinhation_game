76e88333c269baf1810b3f60edb51f5e23347ec8
OM <orlandomedeiros.junior@gmail.com>
2023-05-24 20:36:11 -0300

    Loop in python

--- index.py
+++ index.py
@@ -2,23 +2,28 @@ print('*******************')
 print('Jogo de advinhação')
 print('*******************')
 
-secret_number = 42
-kick_str = input('Digite o seu número: ')
+total_rounds = 3
+round = 1
 
-print('Você digitou', kick_str)
+while(round <= total_rounds):
+    secret_number = 42
+    kick_str = input('Digite o seu número: ')
 
-kick = int(kick_str)
-hit = secret_number == kick
-highest = secret_number < kick
-smaller = secret_number > kick
+    print('Você digitou', kick_str)
 
-if (hit):
-    print("Você acertou!")
-else:
-    if(highest):
-        print("Você errou! O seu chute foi maior que o número secreto")
-    elif(smaller):
-        print("Você errou! O seu chute foi menor que o número secreto")
+    kick = int(kick_str)
+    hit = secret_number == kick
+    highest = secret_number < kick
+    smaller = secret_number > kick
 
-            
-print("Final do Jogo")
+    if (hit):
+        print("Você acertou!")
+    else:
+        if(highest):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif(smaller):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+    print("Final do Jogo")
+    
+    round = round + 1
