564ab36a6b5fc783c7205d8859f2485d426fb1ef
OM <orlandomedeiros.junior@gmail.com>
2023-05-24 19:57:45 -0300

    Elif becouse else dont accept any value

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-print('__________________')
+print('*******************')
 print('Jogo de advinhação')
-print('▔▔▔▔▔▔▔▔▔▔')
+print('*******************')
 
 secret_number = 42
 kick_str = input('Digite o seu número: ')
@@ -8,10 +8,17 @@ kick_str = input('Digite o seu número: ')
 print('Você digitou', kick_str)
 
 kick = int(kick_str)
+hit = secret_number == kick
+highest = secret_number < kick
+smaller = secret_number > kick
 
-if (secret_number == kick):
+if (hit):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(highest):
+        print("Você errou! O seu chute foi maior que o número secreto")
+    elif(smaller):
+        print("Você errou! O seu chute foi menor que o número secreto")
 
+            
 print("Final do Jogo")
